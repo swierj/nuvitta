05ffe9b87bf661b6ee677a9f22c1e77be4d36b0b
Jacob Swierz <swierj@uw.edu>
2024-01-06 09:53:57 -0800

    added contact form page, more products, updated home page, stripe payments

--- server/server.js
+++ server/server.js
@@ -1,23 +1,26 @@
-require('dotenv').config()
-
 const express = require('express')
 const app = express()
+require('dotenv').config()
 
 app.use(express.json())
-app.use(express.static('build'))
+app.use(express.static(process.env.STATIC_DIR))
 
 const stripe = require('stripe')(process.env.STRIPE_SK)
 
-const DOMAIN = 'http://localhost:3000'
+const testDOMAIN = 'http://localhost:3000'
+
+//i dont think I need this
+/* app.get('/', (req, res) => {
+  const path = resolve(process.env.STATIC_DIR + '/index.html')
+  res.sendFile(path)
+}) */
 
 // add try catch
 app.post('/create-checkout-session', async (req, res) => {
-  console.log('got inside')
   const items = req.body.items
-  console.log(items)
   let lineItems = []
+  // adding cart item data to the lineItems array for the format that stripe wants
   items.forEach((item) => {
-    console.log(item.price_id)
     lineItems.push({
       price_data: {
         currency: 'usd',
@@ -29,13 +32,19 @@ app.post('/create-checkout-session', async (req, res) => {
       quantity: item.amount,
     })
   })
-  console.log('line items ******')
-  console.log(lineItems)
   const session = await stripe.checkout.sessions.create({
     ui_mode: 'embedded',
     line_items: lineItems,
     mode: 'payment',
-    return_url: `${DOMAIN}/return?session_id={CHECKOUT_SESSION_ID}`,
+    return_url: `${process.env.DOMAIN}/return?session_id={CHECKOUT_SESSION_ID}`,
+    billing_address_collection: 'required',
+    shipping_address_collection: { allowed_countries: ['US', 'CA'] },
+    custom_text: {
+      submit: {
+        message:
+          'You will recieve a payment reciept and confirmation in your email.',
+      },
+    },
   })
 
   res.send({ clientSecret: session.client_secret })
@@ -50,6 +59,4 @@ app.get('/session-status', async (req, res) => {
   })
 })
 
-console.log('test')
-
-app.listen(3000)
+app.listen(5000, console.log('Running on port 5000.'))
